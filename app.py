85af66d066203bd21eaad9f7e62a25a491e6ff1c
Kwitsatz <86874918+KeyTetra@users.noreply.github.com>
2024-09-11 20:53:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -90,12 +90,13 @@ def mission():
 #----------------------------------------------------------------------------#
 
 # Default port:
+"""
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)"""
 
 # Or specify port manually:
-'''
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-'''
+
